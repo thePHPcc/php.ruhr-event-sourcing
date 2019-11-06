fff5186ecde9b11a315a946c7e46ec92547d3c4e
Arne Blankerts <Arne@Blankerts.de>
2019-11-06 10:19:25 +0100

    Add mock services and core domain code

---
+++ src/domain/CartItemCollection.php
@@ -0,0 +1,20 @@
+<?php declare(strict_types = 1);
+namespace Eventsourcing;
+
+class CartItemCollection implements \IteratorAggregate, \Countable {
+
+    /** @var CartItem[] */
+    private $items = [];
+
+    public function add(CartItem $cartItem): void {
+        $this->items[] = $cartItem;
+    }
+
+    public function count(): int {
+        return \count($this->items);
+    }
+
+    public function getIterator(): \ArrayIterator {
+        return new \ArrayIterator($this->items);
+    }
+}
